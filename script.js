7399c7a25b35c0dfc14438556b80523414f40972
mmehtapavci <mmmehtapavci@gmail.com>
2023-09-29 21:15:08 +0300

    Weather Js End

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ const setQuery = (e) => {
 
 const getResult = (cityName) => {
     let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
-    console.log(query)
+    fetch(query)
         .then(weather => {
             return weather.json()
         })
         .then(displayResult)
 }
 
-const searchBar = document.getElementById('searcBar')
+const displayResult = (result) => {
+    let city = document.querySelector('.city')
+    city.innerText = `${result.name}, ${result.sys.country}`
+
+    let temp = document.querySelector('.temp')
+    temp.innerText = `${Math.round(result.main.temp)}°C`
+
+    let desc = document.querySelector('.desc')
+    desc.innerText = result.weather[0].description
+
+    let minmax = document.quqerySelector('.minmax')
+    minmax.innerText = `${Math.round(result.main.temp.temp_min)}°c
+    ${Math.round(result.main.temp.temp_max)}°c`
+}
+
+
+const searchBar = document.getElementById('searchBar')
 searchBar.addEventListener('keypress', setQuery)
